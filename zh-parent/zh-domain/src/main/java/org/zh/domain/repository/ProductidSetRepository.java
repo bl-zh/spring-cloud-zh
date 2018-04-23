9ac37ae0b06f7b8aef96756a87062202f146db28
sunsun2018 <287633268@qq.com>
2018-04-23 17:55:25 +0800

    提交代码

---
+++ zh-parent/zh-domain/src/main/java/org/zh/domain/repository/ProductidSetRepository.java
@@ -0,0 +1,8 @@
+package org.zh.domain.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.zh.domain.entity.ProductidSet;
+
+public interface ProductidSetRepository extends CrudRepository<ProductidSet, Integer>{
+
+}
