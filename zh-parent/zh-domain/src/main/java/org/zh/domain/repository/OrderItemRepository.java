a404d187eb3cab763dd1c79191bc3ae540f082f5
sunsun2018 <287633268@qq.com>
2018-04-23 17:11:43 +0800

    提交代码

--- zh-parent/zh-domain/src/main/java/org/zh/domain/repository/OrderItemRepository.java
+++ zh-parent/zh-domain/src/main/java/org/zh/domain/repository/OrderItemRepository.java
@@ -6,7 +6,19 @@ import org.zh.domain.entity.OrderItem;
 
 public interface OrderItemRepository extends CrudRepository<OrderItem, Integer>{
 	
-	@Query("select a.* from order_item a,order b where a.order_id = b.id and b.user_id = ?1")
-	Iterable<OrderItem> findOrderItemByUserId(Integer userId);
+	/**
+	 * 查询订单
+	 * @param userId
+	 * @return
+	 */
+	@Query("select id,order_id,order_no,product_id,sale_price,quantity,create_time,amount,batch_code,product_code from order_item  where order_id = ?1 ")
+	Iterable<OrderItem> findOrderItemByUserIdNew(int orderId);
+	/**
+	 * 查询所有的
+	 * @param userId
+	 * @return
+	 */
+	@Query("select a.id,a.order_id,a.order_no,a.product_id,a.sale_price,a.quantity,a.create_time,a.amount,a.batch_code,a.product_code from order_item a,order b where a.order_id = b.id and b.userid = ?1")
+	Iterable<OrderItem> findOrderItemByUserId(int userId);
 	
 }
--- zh-parent/zh-domain/src/main/java/org/zh/domain/repository/OrderRepository.java
+++ zh-parent/zh-domain/src/main/java/org/zh/domain/repository/OrderRepository.java
@@ -1,8 +1,15 @@
 package org.zh.domain.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.zh.domain.entity.Order;
 
 public interface OrderRepository extends CrudRepository<Order, Integer>{
-
+	/**
+	 * 查询最新的订单
+	 * @param userId
+	 * @return
+	 */
+	@Query("select id,order_no,userid,create_time,total from order where userid = ?1 order by CREATE_time desc limit 1 ")
+	Order findOrderLast(int userid);
 }
