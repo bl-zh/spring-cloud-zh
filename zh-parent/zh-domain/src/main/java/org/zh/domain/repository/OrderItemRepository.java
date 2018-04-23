98bd9e3d2e5940dbfe9f0a08915337e39d270163
sunsun2018 <287633268@qq.com>
2018-04-23 19:23:22 +0800

    提交代码

--- zh-parent/zh-domain/src/main/java/org/zh/domain/repository/OrderItemRepository.java
+++ zh-parent/zh-domain/src/main/java/org/zh/domain/repository/OrderItemRepository.java
@@ -1,7 +1,5 @@
 package org.zh.domain.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.zh.domain.entity.OrderItem;
--- zh-parent/zh-domain/src/main/java/org/zh/domain/repository/OrderRepository.java
+++ zh-parent/zh-domain/src/main/java/org/zh/domain/repository/OrderRepository.java
@@ -13,8 +13,8 @@ public interface OrderRepository extends CrudRepository<Order, Integer>{
 	 * @param userId
 	 * @return
 	 */
-//	@Query("select a from order a where a.userid = ?1 order by CREATE_time desc limit 1 ")
-//	Order findOrderLast(int userid);
+	@Query("select a from Order a where a.userid = ?1 ")
+	List<Order> findOrderByUserid(int userid);
 	@Query(value="select o from Order o where o.createTime <?1")
 	List<Order> findByCreateTimeLessThen(Date createTime);
 }
