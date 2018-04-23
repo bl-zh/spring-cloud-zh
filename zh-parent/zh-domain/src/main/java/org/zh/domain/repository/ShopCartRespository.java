6b94cca4319a5e246a688dd5fc0d56a830b5131c
hudepin <hudepin@163.com>
2018-04-23 10:31:55 +0800

    add shopcart respository

---
+++ zh-parent/zh-domain/src/main/java/org/zh/domain/repository/ShopCartRespository.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package org.zh.domain.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.zh.domain.entity.ShopCart;
+
+/**
+ * @author hudepin
+ *
+ */
+public interface ShopCartRespository  extends CrudRepository<ShopCart,Integer>{
+
+}
